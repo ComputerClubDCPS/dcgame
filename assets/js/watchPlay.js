d0d1aace03083803cfcac92c24af0619f74636ee
tf7software <rhenrywarren@gmail.com>
2025-01-07 07:43:57 -0700

    No ads! Whoohooooooooo

--- assets/js/watchPlay.js
+++ assets/js/watchPlay.js
@@ -9,9 +9,9 @@ var episode = null;
 if (type == 'tv') {
     season = urlParams.get('season');
     episode = urlParams.get('episode');
-    document.getElementById('movieIframe').src = `https://vidsrc.icu/embed/tv/${id}/${season}/${episode}`;
+    document.getElementById('movieIframe').src = `https://vidsrc.su/embed/tv/${id}/${season}/${episode}`;
 } else {
-    document.getElementById('movieIframe').src = `https://vidsrc.icu/embed/movie/${id}`;
+    document.getElementById('movieIframe').src = `https://vidsrc.su/embed/movie/${id}`;
 }
 
 
