9885465f5136f81b4a582f05d2c26ce22c95adb0
tf7software <rhenrywarren@gmail.com>
2025-01-11 16:55:37 -0700

    Update Bare

--- proxy/uv/uv.config.js
+++ proxy/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/proxy/active/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/proxy/uv/uv.handler.js',
