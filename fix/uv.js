3b387312a94f10f8f96ce2bc7c991e5a646f583e
tf7software <rhenrywarren@gmail.com>
2025-01-27 17:24:47 -0700

    maybe fix

---
+++ fix/uv.js
@@ -0,0 +1,8 @@
+importScripts('/embed/uv/uv.bundle.js');
+importScripts('/embed/uv/uv.config.js');
+importScripts('/embed/uv/uv.sw.js');
+importScripts('https://arc.io/arc-sw-core.js');
+
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
