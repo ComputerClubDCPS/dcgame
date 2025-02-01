7b8f144a304bf44c15ee3bbdba99b837ac845a00
tf7software <rhenrywarren@gmail.com>
2025-01-31 17:03:47 -0700

    embed

---
+++ e/uv/uv.config.js
@@ -0,0 +1,13 @@
+self.__uv$config = {
+    prefix: '/embed/',
+    
+    // BARE SERVER
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
+    
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/e/uv/uv.handler.js',
+    bundle: '/e/uv/uv.bundle.js',
+    config: '/e/uv/uv.config.js',
+    sw: '/e/uv/uv.sw.js',
+};
