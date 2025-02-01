c5d04dbe0442c55c281bb8c62c0310726a90fc10
tf7software <rhenrywarren@gmail.com>
2025-02-01 16:00:47 -0700

    get rid of /e/

--- e/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/e/uv/uv.bundle.js');
-importScripts('/e/uv/uv.config.js');
-importScripts('/e/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
